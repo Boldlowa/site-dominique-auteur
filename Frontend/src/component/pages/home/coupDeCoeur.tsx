a387eb6f8f04cbcbbce4c6504ac676d22df8cf02
Boldlowa <blanchette.ludovic@yahoo.ca>
2026-02-04 23:14:12 -0500

    FCT Ajout d'un theme global

--- Frontend/src/component/pages/home/coupDeCoeur.tsx
+++ Frontend/src/component/pages/home/coupDeCoeur.tsx
@@ -1,11 +1,11 @@
-import { Card } from "@mui/material";
+import { Card, Typography } from "@mui/material";
 import type { Livre } from "../../../models";
 
 export function CoupDeCoeur( { favoriteList }: { favoriteList: Livre[] } ) {
   return (
-    <Card>
-      <h2>Coup de Coeur</h2>
-      <p>Découvrez mes œuvres préférées et les raisons de mon attachement.</p>
+    <Card sx={{ padding:2}}>
+      <Typography variant="h4">Mes Coups de Coeur</Typography>
+      <Typography variant="h6">Sélection spéciale d'œuvres qui me tiennent à cœur</Typography>  
     </Card>
   );
 }
